51aa2085d0bfed54e2e4993f91400e6f79dd19bb
Boleslav Horak <boleslavh@gmail.com>
2025-03-17 13:51:11 +0100

    Re-added stackpool version of StringBuilderPool

---
+++ Runtime/StringBuilderPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace SeweralIdeas.Pooling
+{
+#if UNITY_5_3_OR_NEWER
+    [UnityEngine.Scripting.Preserve]
+#endif
+    public class StringBuilderPool : StackPool<StringBuilder, StringBuilderPool>
+    {
+        protected override StringBuilder Alloc()
+        {
+            return new StringBuilder();
+        }
+
+        protected override void Finalize(StringBuilder obj)
+        {
+            obj.Clear();
+        }
+
+        protected override void Prepare(StringBuilder obj)
+        {
+            obj.Clear();
+        }
+    }
+}
